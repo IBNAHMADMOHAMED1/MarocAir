b34340c7eb1d284405b6ba6c3270c747520a226a
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-11 14:25:09 +0100

    model flight

--- src/main/java/com/marocair/models/model/Flight.java
+++ src/main/java/com/marocair/models/model/Flight.java
@@ -1,4 +1,23 @@
 package com.marocair.models.model;
+import java.sql.*;
+import java.util.List;
 
-public class Flight {
+public class Flight extends Model {
+    private String id;
+    private String departure;
+    private String arrival;
+    private String date;
+    private String price;
+    private static List<Flight> flights;
+
+    public Flight() {
+        super();
+        setTableName("flight");
+    }
+    // get all flights
+    public List<Flight> getFlights() throws SQLException {
+        ResultSet rs = getAll();
+        return flights;
+    }
 }
+
