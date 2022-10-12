e352cd6a4816f6d46f8424088ecc44faeb90462d
Zakaria-Tajer <za.tajer@gmail.com>
2022-10-12 12:23:36 +0100

    adding drivers

--- src/main/java/com/marocair/models/dao/DBConnection.java
+++ src/main/java/com/marocair/models/dao/DBConnection.java
@@ -2,11 +2,13 @@ package com.marocair.models.dao;
 import java.sql.*;
 public class DBConnection {
     // database connection
-    private static final String DB_NAME = "mf";
+    private static final String DB_NAME = "marocair";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "123456";
 
     private static final String URL = "jdbc:mysql://localhost:3306/" + DB_NAME ;
+
+
     // method returns a connection to the database
     public static Connection getConnection() throws SQLException {
         System.out.println("Connecting to database...");
